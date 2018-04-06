6815625d1c82e27f19c74824ce0e95323f61d6f5
Xiaoxiao1234567890 <rosaliu.567@gmail.com>
2018-04-07 00:14:33 +0800

    Examing Edit Methods and Edit View P1

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace MvcMovie.Models
@@ -7,6 +8,10 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name ="Release Date")] // The Display attribute specifies what to display for the name of a field (in this case "Release Date" instead of "ReleaseDate").
+        [DataType(DataType.Date)] //The DataType attribute specifies the type of the data, in this case it's a date, so the time information stored in the field is not displayed. 
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)] // The DisplayFormat attribute is needed for a bug in the Chrome browser that renders date formats incorrectly.
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
